c869d06282cc226d2174c6b53bcaf8a14583101d
Ayşe Abdullahoğlu <ayse.abdullahoglu@yandex.com>
2024-04-09 21:58:20 +0300

    add userTests

--- tests/userTests.js
+++ tests/userTests.js
@@ -1,13 +1,14 @@
-const chakram = require('chakram');
+const chakram = require('chakram'), expect= chakram.expect;
 
 describe("User Tests", function() {
 
     it("GET /user/{username} Get User Info", function(){
-    
+    const response = chakram.post("https://petstore.swagger.io/v2/user/testqa41");
+    expect(response).to.have.status(200);
     });
 
-    it("TC0002 /v2/user/{userName}", function(){
+ //   it("TC0002 /v2/user/{userName}", function(){
         
-    });
+ //   });
 
 });
