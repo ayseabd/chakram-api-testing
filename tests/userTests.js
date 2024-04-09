d6878e3ede3615bc02b553084206fc5b4febc85a
Ayşe Abdullahoğlu <ayse.abdullahoglu@yandex.com>
2024-04-09 18:44:34 +0300

    add project

---
+++ tests/userTests.js
@@ -0,0 +1,13 @@
+const chakram = require('chakram');
+
+describe("User Tests", function() {
+
+    it("GET /user/{username} Get User Info", function(){
+    
+    });
+
+    it("TC0002 /v2/user/{userName}", function(){
+        
+    });
+
+});
